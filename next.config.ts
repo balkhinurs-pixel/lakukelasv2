3007abde92197197f46032a929cc83c42c9868b1
Balkhi Nurs <Balkhinurs@gmail.com>
2025-08-08 14:07:40 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,36 @@
+
 import type {NextConfig} from 'next';
 
 const withPWA = require('next-pwa')({
   dest: 'public',
   register: true,
   skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development'
+  disable: process.env.NODE_ENV === 'development',
+  runtimeCaching: [
+    {
+      urlPattern: ({ url }) => true,
+      handler: 'NetworkFirst',
+      options: {
+        cacheName: 'all-cache',
+        networkTimeoutSeconds: 10,
+        expiration: {
+          maxEntries: 100,
+          maxAgeSeconds: 30 * 24 * 60 * 60, // 30 days
+        },
+        cacheableResponse: {
+          statuses: [0, 200],
+        },
+        fetchOptions: {
+          mode: 'no-cors',
+        },
+        // This is the fix
+        handlerDidFetch: async ({url, response}: {url: URL; response: Response}) => {
+          url.search = '';
+          return response;
+        }
+      },
+    },
+  ],
 })
 
 const nextConfig: NextConfig = {
