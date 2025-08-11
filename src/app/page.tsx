ab4bbf1463e1046166e6babf42d2bff31534dbd4
Balkhi Nurs <Balkhinurs@gmail.com>
2025-08-11 08:47:53 +0000

    masih persegi dan kecil , coba kamu periksa

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -165,7 +165,7 @@ export default function LoginPage() {
     return (
         <div className="bg-primary text-primary-foreground p-8 md:p-12 flex flex-col justify-center items-center text-center">
             <div className="mb-8">
-               <Image src="/login.png" alt="LakuKelas Logo" width={280} height={60} className="w-auto h-16" />
+               <Image src="/login.png" alt="LakuKelas Logo" width={280} height={60} className="w-72 h-auto" />
             </div>
             <h1 className="text-3xl font-bold font-headline mb-2">
                 {titles[view]}
@@ -299,6 +299,7 @@ export default function LoginPage() {
     </div>
   );
 }
+
 
 
 
