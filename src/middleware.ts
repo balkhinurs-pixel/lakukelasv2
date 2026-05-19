2e2ca455825afaf246f3d4eaccdfb97106f34961
BNS Art <balkhi05@gmail.com>
2026-05-19 11:07:17 +0000

    error saat build vercel 17:59:15.926 Running build in Washington, D.C.,

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,6 @@
 
 import { createServerClient, type CookieOptions } from '@supabase/ssr'
-import { NextResponse, type NextRequest } from 'next/request'
+import { NextResponse, type NextRequest } from 'next/server'
 
 /**
  * Optimized Middleware:
